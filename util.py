a05e4319c45f69ff4648eda96e17e93e7167ab51
greg <greg@gsidier.com>
2012-08-29 20:59:25 +0100

    add utility function to load greyscale images

--- util.py
+++ util.py
@@ -5,6 +5,9 @@ import numpy
 def cvim2array(im):
 	return numpy.array(im)[:,:,::-1]
 
+def cv2grey(im):
+	return numpy.array(im)[:,:,0].astype(float) / 256.
+
 def cvimshow(im):
 	imshow(cvim2array(im))
 
