046adc6f41b1a220e964bf224c3dde6cc0ed1063
greg <greg@gsidier.com>
2012-09-18 20:50:47 +0100

    add functions to load 16-bit greyscale images and masks

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 import cv
-from pylab import imshow
+from pylab import imread, imshow
+from PIL import Image
 import numpy
 
 def cvim2array(im):
@@ -11,4 +12,19 @@ def cv2grey(im):
 def cvimshow(im):
 	imshow(cvim2array(im))
 
+def loadmask(path):
+	im = imread(path)
+	if im.shape == 3:
+		im = im[:,:,0]
+	im = im.astype(bool)
+	return im
+
+def loadgrey16(path):
+	im = Image.open(path)
+	s = im.tostring()
+	w, h = im.size
+	if len(s) != w * h * 2:
+		raise TypeError
+	a = numpy.fromstring(s, numpy.uint16)
+	m = a.reshape((w, h))
 
