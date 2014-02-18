71c45f50cf81dbd15df0edf2ab908ad2c2fb050a
Gregory Sidier <ffoct@gsidier.com>
2014-02-18 19:32:13 +0000

    update trainingdata.py paths

--- trainingdata.py
+++ trainingdata.py
@@ -1,8 +1,8 @@
 TRAINING_DATA = {
 	"11H00940A-5666c9.to8bit.resize-979-675-ANTIALIAS.tif": {
-		'fibrous': "11H00940A.MASK-fibrous.resize-979-675.tif",
+		'fibrous': "11H00940A.MASK-fibrous.resize-979-675.png",
 	},
 	"11H01173A-f34ab7.to8bit.resize-976-975-ANTIALIAS.tif": {
-		'tissue': "11H01173A.MASK-tissue.resize-976-975.tif",
+		'tissue': "11H01173A.MASK-tissue.resize-976-975.png",
 	},
 }
